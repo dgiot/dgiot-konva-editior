fe88cf8a1f166040f05f5fcf7edc11178d0bf43b
h7ml <h7ml@qq.com>
2021-10-18 15:53:35 +0800

    feat(:tada:): initialize the project

---
+++ src/custom-component/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+
+const components = ['Picture', 'VText', 'VButton', 'Group', 'RectShape'];
+
+components.forEach((key) => {
+    Vue.component(key, () => import(`@/custom-component/${key}`));
+});
