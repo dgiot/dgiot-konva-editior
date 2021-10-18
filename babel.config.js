aa8be66f8a4e61ba25239898347edd1bf6e98c7f
h7ml <h7ml@qq.com>
2021-10-18 16:01:42 +0800

    fix(babel.config.js && package.json): fix compilation failure caused by eslint detection

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+let plugins = ['dynamic-import-node'];
+if (process.env.NODE_ENV !== 'development') {
+    plugins.push('transform-remove-console');
+}
 module.exports = {
     presets: ['@vue/cli-plugin-babel/preset'],
+    plugins,
 };
