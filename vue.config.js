b72f8e4199aa49b7c9bb59c88f6efe0238f1680a
h7ml <h7ml@qq.com>
2021-10-18 16:23:45 +0800

    feat(fix compilation failure caused by eslint detection): fix compilation failure caused by eslint

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,21 @@
-const CompressionPlugin = require('compression-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin')
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
     publicPath: isProd ? '/dgiot-konva-editior/' : './',
     // eslint-disable-next-line consistent-return
-    configureWebpack: () => {
+    outputDir: 'dist', // 输出文件目录
+    assetsDir: 'static', // 放置生成的静态文件目录（js css img）
+    productionSourceMap: false, // 生产环境是否生成 sourceMap 文件
+    // eslint-disable-next-line consistent-return
+    configureWebpack: (config) => {
+        config.mode = 'production'
+        config.performance = {
+            // 打包文件大小配置
+            maxEntrypointSize: 10000000,
+            maxAssetSize: 30000000,
+        }
         if (isProd) {
             return {
                 plugins: [
@@ -15,7 +25,7 @@ module.exports = {
                         deleteOriginalAssets: false, // 是否删除原文件
                     }),
                 ],
-            };
+            }
         }
     },
-};
+}
